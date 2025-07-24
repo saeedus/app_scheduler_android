f376185cfc4d35f198340b0bd2371870318db79f
Saeedus Salehin <saeedussalehin22@gmail.com>
2025-07-24 19:08:24 +0600

    Implemented bottom nav bar for switching between all apps and scheduled apps screens

--- app/src/main/java/com/sazim/appscheduler/schedule/presentation/ScheduleActivity.kt
+++ app/src/main/java/com/sazim/appscheduler/schedule/presentation/ScheduleActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.sazim.appscheduler.schedule.presentation.navigation.BottomNavBar
 import com.sazim.appscheduler.schedule.presentation.navigation.ScheduleNavGraph
 import com.sazim.appscheduler.schedule.presentation.navigation.ScheduleNavRoutes
 import org.koin.androidx.compose.koinViewModel
@@ -20,12 +21,15 @@ class ScheduleActivity : ComponentActivity() {
         setContent {
             val viewModel = koinViewModel<ScheduleViewModel>()
             val state by viewModel.state.collectAsState()
+            val navController = rememberNavController()
 
 
-            Scaffold { innerPadding ->
+            Scaffold(bottomBar = {
+                BottomNavBar(navController = navController)
+            }) { innerPadding ->
                 ScheduleNavGraph(
                     modifier = Modifier.padding(innerPadding),
-                    navController = rememberNavController(),
+                    navController = navController,
                     startDestination = ScheduleNavRoutes.AppsScreen.route,
                     viewModel = viewModel,
                     state = state
---
+++ app/src/main/java/com/sazim/appscheduler/schedule/presentation/navigation/model/BottomNavItem.kt
@@ -0,0 +1,8 @@
+package com.sazim.appscheduler.schedule.presentation.navigation.model
+
+data class BottomNavItem(
+    val route: String,
+    val iconRes: Int,
+    val label: String
+)
+
